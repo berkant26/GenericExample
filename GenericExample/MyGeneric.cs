a3de8f099c3b43455b9bbd19b923f54718fc8446
berkant26 <youbeko26@gmail.com>
2021-08-17 00:40:33 +0300

    Add project files.

---
+++ GenericExample/MyGeneric.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExample
+{
+    class MyGeneric<T>
+    {
+        T[] _tempArray;
+        T[] _array;
+        public MyGeneric()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+
+            }
+
+            _array[_array.Length - 1] = item;
+
+
+            Console.WriteLine("Added ctiy :"+" "+item);
+        }
+
+        public int Count        
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
---
+++ GenericExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<string> citys = new MyGeneric<string>();
+            citys.Add("Eskisehir");
+            citys.Add("New york");
+            citys.Add("Dortmund");
+            Console.WriteLine(citys.Count);
+            
+        }
+    }
+}
